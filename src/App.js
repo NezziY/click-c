f35f246ea2962714701bced1e971ca5890d04985
VaneSanchez <nyagami@yahoo.com>
2023-04-11 16:03:02 -0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const ClickAdd = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const ClickSubstract = () => {
+    if (numClicks === 0) {
+      alert("Error!");
+      return;
+    }
+    setNumClicks(numClicks - 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='title-container'>
+        <h1>Contador de Clicks</h1>
+      </div>
+      <div className='contenedor-contador'>
+        <Contador numClicks={numClicks} />
+
+        <div>
+          <Boton
+          texto='+'
+          esBotonDeClick={true}
+          manejarClick={ClickAdd} />
+
+          <Boton
+            texto='-'
+            esBotonDeClick={true}
+            manejarClick={ClickSubstract} />
+        </div>
+        
+
+        <Boton
+          texto='Reset'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador} />
+      </div>
     </div>
   );
 }
